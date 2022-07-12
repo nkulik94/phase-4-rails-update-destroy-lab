a9488b76661b30e033de2edd5e9419c810f5061d
Naftali Kulik <nkulik1994@gmail.com>
2022-07-12 14:39:01 -0400

    commit tree 7cd62d9a2a222fdb19384cedb63efacacded70c0

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -8,7 +9,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = Plant.find(params[:id])
     render json: plant
   end
 
@@ -18,7 +19,24 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find(params[:id])
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  def destroy
+    plant = Plant.find(params[:id])
+    plant.destroy
+    head :no_content
+  end
+
+
   private
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
